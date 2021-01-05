ce0558e53f6188c83a95c86fed778a404bd8388f
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-05 23:54:37 +0530

    Update TodoController

--- src/main/java/com/rajrajhans/SpringTodoApp/controllers/TodoController.java
+++ src/main/java/com/rajrajhans/SpringTodoApp/controllers/TodoController.java
@@ -1,11 +1,11 @@
 package com.rajrajhans.SpringTodoApp.controllers;
 
 import com.rajrajhans.SpringTodoApp.repositories.TodoRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller                                         // telling Spring that this is a Controller
+@RestController                                     // telling Spring that this is a Controller for a REST API
 public class TodoController {
 
     private final TodoRepository todorepo;          // Adding a "dependency" on the TodoRepository
@@ -19,6 +19,6 @@ public class TodoController {
 
         model.addAttribute("todos", todorepo.findAll());    // At runtime, when Spring gets a request at /todos, it is going to execute the getTodos method and it's going to provide that method a Model object, and our code is going to add a attribute called "todos" to that model by executing a findALl() on todorepo (which will give us a list of todos). Then, this model will return back to our view layer, and it is going to have an attribute called todos on it, then we'll be able to utilize that value inside of our view layer
 
-        return "todos";
+        return "todos1";
     }
 }
