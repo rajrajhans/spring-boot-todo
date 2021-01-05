ebdcf63800c93b3a2ad35c4f37971b8c2aea9c9e
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-05 10:58:45 +0530

    Set up a Spring Boot Project

---
+++ src/main/java/com/rajrajhans/SpringTodoApp/SpringTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.rajrajhans.SpringTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajrajhans/SpringTodoApp/SpringTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajrajhans.SpringTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
