f273276bcbbf83374d1348fb695f68aa4042abea
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-06 14:12:12 +0530

    Create a TodoService and use it in TodoController

--- src/main/java/com/rajrajhans/SpringTodoApp/controllers/TodoController.java
+++ src/main/java/com/rajrajhans/SpringTodoApp/controllers/TodoController.java
@@ -1,24 +1,33 @@
 package com.rajrajhans.SpringTodoApp.controllers;
-
+import com.rajrajhans.SpringTodoApp.domains.Todo;
 import com.rajrajhans.SpringTodoApp.repositories.TodoRepository;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rajrajhans.SpringTodoApp.services.TodoService;
+import org.springframework.web.bind.annotation.*;
 
-@RestController                                     // telling Spring that this is a Controller for a REST API
+import java.util.List;
+
+@RestController                                                    // telling Spring that this is a Controller for a REST API
 public class TodoController {
 
-    private final TodoRepository todorepo;          // Adding a "dependency" on the TodoRepository
+    private final TodoService todoService;                          // Adding a "dependency" on the TodoService
 
-    public TodoController(TodoRepository todorepo) {// Now when Spring instantiates this controller, it will also inject a TodoRepository instance
-        this.todorepo = todorepo;
+    public TodoController(TodoRepository todorepo, TodoService todoService) { // Now when Spring instantiates this controller, it will also inject a TodoRepository instance
+        this.todoService = todoService;
     }
 
-    @RequestMapping("/todos")                    // mapping the route "/todos" to getTodos
-    public String getTodos(Model model){            // The argument is basically a Model that we are going to return back to the view after adding our own things to it
-
-        model.addAttribute("todos", todorepo.findAll());    // At runtime, when Spring gets a request at /todos, it is going to execute the getTodos method and it's going to provide that method a Model object, and our code is going to add a attribute called "todos" to that model by executing a findALl() on todorepo (which will give us a list of todos). Then, this model will return back to our view layer, and it is going to have an attribute called todos on it, then we'll be able to utilize that value inside of our view layer
-
-        return "todos1";
+    @RequestMapping("/todos/{id}")                                // mapping the route "/todos" to getTodos
+    public List<Todo> getTodo(@PathVariable String id) {             // The argument is basically a Model that we are going to return back to the view after adding our own things to it
+        return todoService.getTodo(id);
     }
+
+    @RequestMapping("/todos/")                                      // mapping the route "/todos" to getTodos
+    public List<Todo> getTodos(@PathVariable String id) {              // The argument is basically a Model that we are going to return back to the view after adding our own things to it
+        return todoService.getTodos();
+    }
+
+    @RequestMapping(method = {RequestMethod.POST}, value = "/todos")
+    public String createTodo(@RequestBody String todo){
+        return todoService.createTodo(todo);
+    }
+
 }
---
+++ src/main/java/com/rajrajhans/SpringTodoApp/services/TodoService.java
@@ -0,0 +1,34 @@
+package com.rajrajhans.SpringTodoApp.services;
+
+import com.rajrajhans.SpringTodoApp.domains.Todo;
+import com.rajrajhans.SpringTodoApp.repositories.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TodoService {
+    // CRUD Methods for our resource here
+    private final TodoRepository todorepo;          // Adding a "dependency" on the TodoRepository
+
+    public TodoService(TodoRepository todorepo) {
+        this.todorepo = todorepo;
+    }
+
+    public List<Todo> getTodo(String id) {
+        List<Todo> allTodos = new ArrayList<>();
+
+        return allTodos;
+    }
+
+    public List<Todo> getTodos() {
+        List<Todo> allTodos = new ArrayList<>();
+        todorepo.findAll().forEach(allTodos::add);
+        return allTodos;
+    }
+
+    public String createTodo(String todo){
+        return todo + " created";
+    }
+}
