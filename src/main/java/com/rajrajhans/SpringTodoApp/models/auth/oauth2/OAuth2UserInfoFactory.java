17f994f5c90f469754ac791c5324e8329dd8f1f2
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-10 19:13:39 +0530

    Add Google as OAuth2.0 Provider

--- src/main/java/com/rajrajhans/SpringTodoApp/models/auth/oauth2/OAuth2UserInfoFactory.java
+++ src/main/java/com/rajrajhans/SpringTodoApp/models/auth/oauth2/OAuth2UserInfoFactory.java
@@ -9,7 +9,10 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOauth2UserInfo(String regID, Map<String, Object> attributes){
         if(regID.equalsIgnoreCase(AuthProvider.github.toString())){
             return new GithubOAuth2UserInfo(attributes);
+        } else if(regID.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
         }else{
+            System.out.println("Returning NULL from oAuthUserInfoFactory");
             return null;
         }
     }
