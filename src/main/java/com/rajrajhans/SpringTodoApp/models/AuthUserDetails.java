b7b6b15d6265225eedb7d78a0b4d26bc631597be
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-10 15:51:51 +0530

    Update Models for integrating OAuth

--- src/main/java/com/rajrajhans/SpringTodoApp/models/AuthUserDetails.java
+++ src/main/java/com/rajrajhans/SpringTodoApp/models/AuthUserDetails.java
@@ -3,27 +3,39 @@ package com.rajrajhans.SpringTodoApp.models;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class AuthUserDetails implements UserDetails {
+public class AuthUserDetails implements OAuth2User, UserDetails {
+    private Long id;
     private String username;
     private String password;
     private boolean active;
     private List<GrantedAuthority> authority;
+    private Map<String, Object> attributes;
 
     public AuthUserDetails(User user){
         this.username = user.getUsername();
         this.password = user.getPassword();
         this.active = user.isActive();
-        System.out.println(user.getRole());
         this.authority = Arrays
                 .stream(user.getRole().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
+    }
+
+    public AuthUserDetails(long id, String username, String password, List<GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authority = authorities;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
@@ -59,5 +71,32 @@ public class AuthUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return active;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public static AuthUserDetails create(User user) {
+        List<GrantedAuthority> authorities = Collections.
+                singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new AuthUserDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    public static AuthUserDetails create(User user, Map<String, Object> attributes) {
+        AuthUserDetails userPrincipal = AuthUserDetails.create(user);
+        userPrincipal.setAttributes(attributes);
+        return userPrincipal;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
